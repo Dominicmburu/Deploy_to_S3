b3060299dbb454c72633a1dc485dd84d1fc43acc
Dominicmburu <dominicmburu034@gmail.com>
2025-02-21 23:32:33 +0300

    s3

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,26 +4,19 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      declarations: [AppComponent],  
     }).compileComponents();
   });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(app).toBeTruthy();  
   });
 
   it(`should have the 'my_app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my_app');
-  });
-
-  it('should have title in template', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges(); 
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('my_app'); 
+    expect(app.title).toEqual('my_app');  
   });
 });
